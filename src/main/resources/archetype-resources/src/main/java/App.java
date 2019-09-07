6cf5912246310eebf2c3571563bff0690c3bcd9f
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-07 17:26:56 -0500

    Added lots of skeleton code

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,13 +1,32 @@
 package $package;
 
+import java.nio.file.Paths;
+import com.google.gson.Gson;
+import fpt.verifiers.core.IVerifier;
+import fpt.verifiers.core.VerifierResult;
+import fpt.verifiers.core.FPTDiagnostic;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static Gson gson = new Gson();
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        IVerifier verifier = null;
+        
+        try{
+            var userDirectory = Paths.get(args[0]);
+            var projectDirectory = Paths.get(args[1]);
+            System.out.println(gson.toJson(verifier.Verify(userDirectory,projectDirectory)));
+        }catch(Exception e){
+            System.out.println(
+                gson.toJson(
+                    new VerifierResult(
+                        false, 
+                        new FPTDiagnostic[]{new FPTDiagnostic(e.toString(),FPTDiagnostic.DiagnosticKind.Error)})));
+        }
     }
 }
---
+++ src/main/resources/archetype-resources/src/main/java/Verifier.java
@@ -0,0 +1,14 @@
+package $package;
+
+import fpt.verifiers.core.IVerifier;
+import java.nio.file.Path;
+import fpt.verifiers.core.VerifierResult;
+
+public class Verifier implements IVerifier{
+    @Override
+    public VerifierResult Verify(Path userDirectory, Path projectDirectory){
+        //Start programming here!
+
+        return null;
+    }
+}
